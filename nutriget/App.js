8d1c5f0ae0ab345b345bb1fbd6fb136e602fdc03
Juan <juan.selvan77@gmail.com>
2023-04-12 11:29:40 +0800

    Adding the default files

---
+++ nutriget/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <h1 color="Red">Hi Fam</h1>
+      <input placeholder="What is your name"></input>
+      <Text>BigManTing</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
