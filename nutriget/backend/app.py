77b9f94880ae86560b78ddebd5f0c232c3e3bdf1
Juan <juan.selvan77@gmail.com>
2023-04-13 15:55:33 +0800

    removed expo files

--- nutriget/backend/app.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import date, datetime
