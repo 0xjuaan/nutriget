948748fcdcc8085d1b9095b51be0d10371e9c25b
Juan <juan.selvan77@gmail.com>
2023-05-04 11:03:16 +0800

    added comments to understand _app.js -thanks copilot

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import 'next/styles/globals.css'
+import 'next/styles/globals.css' //This is so that all of my pages have access to the global css file.
+//The _app.js file is speial because it is the top level component which means that it is the parent component of all of the other components.
+//This is already configured and not required for me to manually put <App> for every page
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
