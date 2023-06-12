a3ad0af0b45d8f52c86a8220ab20d867c15e452c
Juan <juan.selvan77@gmail.com>
2023-06-12 17:52:54 +0800

    add the onclick function to the mealList comp

--- components/mealList.js
+++ components/mealList.js
@@ -1,11 +1,16 @@
 import { Inter } from 'next/font/google'
 import styles from 'next/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
-//data is an array: [{meal_id: 8, calories: 619, total_fat: 37.4, total_carbs: 47.76, sugars: 10.28, name:}]
+//data is an array: [{meal_id: 8 (later f2cea6c), calories: 619, total_fat: 37.4, total_carbs: 47.76, sugars: 10.28, name:}]
 
-export default function MealList({data}) {
+export default function MealList({data, function_on_click}) {
     //We pass in all the data
+    if (data == "None") {
+        console.log("No meals meeee")
+        return (<p>You have no meals. <Link href="/scratch">Click here to add your first meal</Link></p>)
+    }
     return (
         <div className={styles.list}>
             {data.map((value) => (
@@ -19,12 +24,10 @@ export default function MealList({data}) {
                     </p>
                     </div>
                     <div className={styles.listCardRight}>
-                        <button id={value.name} className={styles.delete}>Remove</button>
+                        <button id={value.meal_id} onClick={function_on_click} className={styles.delete}>Remove</button>
                     </div>
                 </div>
             ))}
         </div>
-        
-
     )
 }
