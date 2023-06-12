f1ef7361e0cd523c4918d264c4f12f4372ea00f8
Juan <juan.selvan77@gmail.com>
2023-06-12 17:52:04 +0800

    changed handling of 0 meal users

--- pages/history.js
+++ pages/history.js
@@ -3,19 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import styles from 'next/styles/Home.module.css'
-import login from 'next/styles/Login.module.css'
 
 import {useState, useEffect} from 'react';
 import MealList from '../components/mealList'
 import { useRouter } from 'next/router'
 import ReactModal from 'react-modal';
-import MainButton from 'next/components/mainButton'
-import Ingredients from '../components/ingredients'
 
 import { getSession } from "/session";
-
-
-
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,8 +23,9 @@ export default function History({ session }) {
   const [user_id, setUser_id] = useState(user.id);
 
   const [mealData, setMealData] = useState([]);
+  const [change_val, setChange] = useState(0);
 
-  //Fetch this user's meals from the database
+  //Fetch all of this user's meals from the database
   useEffect(() => {
   fetch ('/api/getMeals', {
     method: 'POST',
@@ -38,15 +33,39 @@ export default function History({ session }) {
       body: JSON.stringify({user_id: user_id})
   })
   .then(response => response.json())
-  .then(meal_data => setMealData(meal_data.rows));
-}, [mealData]);
+  .then(meal_data => {
+    if (meal_data.response == 'No meals') {
+      setMealData("None")
+    }
+    else {
+    setMealData(meal_data.rows)
+    console.log(`DATA RETURNED: ${meal_data.rows}`)
+    }
+  });
+}, [change_val]);
+
+
+
+const deleteMeal = (event) => {
+  const meal_id = event.target.id;
+  console.log(`meal is ${meal_id}`);
+  fetch('/api/deleteMeal', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({user_id: user_id, meal_id: meal_id})
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("here is the data: " + data.response)
+    setChange(change_val + 1)});
+}
 
   //Make and import a component that displays the meals in a list
   //First prop: data.rows, which is an array of objects
   //Also pass a function that deletes the meal from the database (based on meal_id)
-  console.log(mealData)
+  console.log("WEWEWE" + JSON.stringify(mealData))
   return (
-  <MealList data={mealData}/>
+  <MealList data={mealData} function_on_click={deleteMeal}/>
   )
 }
 
